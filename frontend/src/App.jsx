eeab2ad710a816e23cf170dc7cb1a63c71386528
Faizy-Dot <160147462+Faizy-Dot@users.noreply.github.com>
2025-08-14 18:43:33 +0500

    check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
       <h1 className="text-4xl font-bold text-green-600">
         Hello Tailwind CSS 🚀
       </h1>
-      <h1>github deployment successfull</h1>
+      <h1>github deployment successfully</h1>
       <img src={logo} alt="Logo"  style={{borderRadius : 25 , height : 100 , width : 100}} />
     </div>
   );
